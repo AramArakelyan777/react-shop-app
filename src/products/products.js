5158420b45e0ae544645cb5e0e89aef6245648f5
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-16 16:33:33 +0400

    created assets

--- src/products/products.js
+++ src/products/products.js
@@ -1,62 +1,73 @@
+import product1 from "../assets/product1.png"
+import product2 from "../assets/product2.jpg"
+import product3 from "../assets/product3.jpg"
+import product4 from "../assets/product4.jpeg"
+import product5 from "../assets/product5.jpg"
+import product6 from "../assets/product6.jpg"
+import product7 from "../assets/product7.jpg"
+import product8 from "../assets/product8.jpg"
+import product9 from "../assets/product9.webp"
+import product10 from "../assets/product10.jpg"
+
 const PRODUCTS = [
     {
         id: 1,
         name: "iPhone",
-        url: "https://varindia.com/uploads/2016/laravel/59fad3ab35f5f.jpg",
+        url: product1,
         cost: 1150,
     },
     {
         id: 2,
         name: "Wireless Mouse",
-        url: "https://img.sellercheck.ru/pic/a4/32/besprovodnaya-mysh-logitech-m185-swift-grey-a4323546f6f214ea73adc7d7970a2ebe-500.jpg",
+        url: product2,
         cost: 20,
     },
     {
         id: 3,
         name: "Wireless Headphones",
-        url: "https://www.pricecheckindia.in/wp-content/uploads/2021/06/41Z+Z9eq2PL.jpg",
+        url: product3,
         cost: 120,
     },
     {
         id: 4,
         name: "Kettle Xiaomi",
-        url: "https://m-gorod.ru/image/cache/data/%D0%94%D0%BE%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5%20%D1%84%D0%BE%D1%82%D0%BE/photo_cha1-500x500.jpeg",
+        url: product4,
         cost: 50,
     },
     {
         id: 5,
         name: "Phillips Shaver",
-        url: "https://m.media-amazon.com/images/I/41CZPBg0LrL._SL500_.jpg",
+        url: product5,
         cost: 15,
     },
     {
         id: 6,
         name: "USB Cables",
-        url: "https://oimparcial.com.br/app/uploads/2020/01/24103751153232.jpg",
+        url: product6,
         cost: 6,
     },
     {
         id: 7,
         name: "USB Lights",
-        url: "https://cf.shopee.vn/file/efd781457bcb2f4b739a90439b63f827",
+        url: product7,
         cost: 2,
     },
     {
         id: 8,
         name: "iPhone Cases Multiple Colors",
-        url: "https://cdnv2.moovin.com.br/redecelltronics/imagens/produtos/det/capa-original-iphone-7-8-4c60e21cf3d7a7c5971bee6bc06b163f.jpg",
+        url: product8,
         cost: 63,
     },
     {
         id: 9,
         name: "Samsung Computer Screen",
-        url: "https://5.imimg.com/data5/LX/WH/AX/SELLER-80697148/sumsung-led-monitor-500x500.jpg",
+        url: product9,
         cost: 200,
     },
     {
         id: 10,
         name: "AUX Cable",
-        url: "https://optfm.ru/upload/resize_cache/iblock/f61/b5v2nb164tntbsrmedvlfq3a3xw621tz/500_500_1/d543d5a3_ba76_11e8_9fdb_0025907ca6e0_bd4e49ac_b9bb_11e9_a216_38d54775357d.jpg",
+        url: product10,
         cost: 1,
     }
 ]
