273e219fe0a50080d00ac2c9f56b1f992a478981
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-16 13:12:13 +0400

    added cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,24 @@
+import PRODUCTS from "../products/products"
+import "../css/Cart.css"
+
 function Cart() {
     return (
         <div>
-            Cart
+            <h1>Cart Items</h1>
+            {PRODUCTS.map(item => {
+                return (
+                    <div className="cartItem" key={item.id}>
+                        <img className="cartItemImg" src={item.url} alt="cart" />
+                        <div>
+                            <p className="cartItemName">{item.name}</p>
+                            <p className="cartItemCost">${item.cost}</p>
+                        </div>
+                    </div>
+                )
+            })}
+            <div>
+                <h3>Total: $</h3>
+            </div>
         </div>
     )
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,7 +4,7 @@ import { ShopContext } from "./ShopContext"
 import { useContext } from 'react'
 
 function Shop() {
-    const { cartItems, addAnItem, removeAnItem } = useContext(ShopContext)
+    const { cartItems, addAnItem } = useContext(ShopContext)
 
     return (
         <div className="productsContainer">
@@ -14,7 +14,7 @@ function Shop() {
                         <img src={item.url} alt="product" />
                         <h2>{item.name}</h2>
                         <p>Cost: ${item.cost}</p>
-                        <button className="addToCart" onClick={() => addAnItem(item.id)}>Add To Cart</button>
+                        <button className="addToCart" onClick={() => addAnItem(item.id)}>Add To Cart {cartItems[item.id] > 0 && <>({cartItems[item.id]})</>}</button>
                     </div>
                 )
             })}
