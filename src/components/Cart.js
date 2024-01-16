4d0ea6c0d0a9fa922f6fd02c8c83b5a8430d45f4
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-01-16 13:20:30 +0400

    filtered cartItems only the added ones display

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,26 @@
 import PRODUCTS from "../products/products"
+import { ShopContext } from "./ShopContext"
 import "../css/Cart.css"
+import { useContext } from "react"
 
 function Cart() {
+    const { cartItems } = useContext(ShopContext)
+
     return (
         <div>
             <h1>Cart Items</h1>
             {PRODUCTS.map(item => {
-                return (
-                    <div className="cartItem" key={item.id}>
-                        <img className="cartItemImg" src={item.url} alt="cart" />
-                        <div>
-                            <p className="cartItemName">{item.name}</p>
-                            <p className="cartItemCost">${item.cost}</p>
+                if (cartItems[item.id] !== 0) {
+                    return (
+                        <div className="cartItem" key={item.id}>
+                            <img className="cartItemImg" src={item.url} alt="cart" />
+                            <div>
+                                <p className="cartItemName">{item.name}</p>
+                                <p className="cartItemCost">${item.cost}</p>
+                            </div>
                         </div>
-                    </div>
-                )
+                    )
+                } return <></>
             })}
             <div>
                 <h3>Total: $</h3>
